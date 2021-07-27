a4c49f1d189937ecbe9d30f2e5de04d28a07090e
Joe Staller <joestaller23@gmail.com>
2021-07-27 11:46:54 -0400

    format MoviesV2

--- services/MoviesV2.js
+++ services/MoviesV2.js
@@ -4,17 +4,19 @@ const DataLoader = require("dataloader");
 
 module.exports = () => {
   // create a new instance of a DataLoader
-const moviesLoader = new DataLoader(async (keys) => {
-  console.log(`fetching data for movies: ${keys.join(", ")}`);
+  const moviesLoader = new DataLoader(async (keys) => {
+    console.log(`fetching data for movies: ${keys.join(", ")}`);
 
-  // Batch fetch from data store
-  const foundMovies = await API.findByNames(keys);
+    // Batch fetch from data store
+    const foundMovies = await API.findByNames(keys);
 
-  // map keys back to their found values (or `null` if not found)
-  return keys.map((key) =>
-    foundMovies.find((movie) => key.toLowerCase() === movie.title.toLowerCase())
-  );
-});
+    // map keys back to their found values (or `null` if not found)
+    return keys.map((key) =>
+      foundMovies.find(
+        (movie) => key.toLowerCase() === movie.title.toLowerCase()
+      )
+    );
+  });
 
   const findAll = async () => API.findAll();
 
